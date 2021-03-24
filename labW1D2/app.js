158165004e56d20376fba472e212ebc83c4f2978
asgdomhail@gmail.com <63181283+Asget-dev@users.noreply.github.com>
2021-03-23 18:58:16 -0700

    Update app.js

--- labW1D2/app.js
+++ labW1D2/app.js
@@ -5,18 +5,17 @@
 // See tests.js for behavior
 // function nameString(obj) {
 //   return "object has no properties";
-// } 
+// }
 // const obj = {};
 // console.log(nameString(obj));
 
 function nameString(obj) {
-  return"object has one property";
+return "ff";
 }
 const student = {
-    name:"Asgedom",
-};
-for(let key in student){
+    name: 5,
+  };
+  for (let key in student) {
     key;
-}
-  
+  }
 console.log(nameString(student));
